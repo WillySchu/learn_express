d81f7ebe0adcdb812cf9b78dfb29caafe178b472
Will Schumacher <meintodengel@gmail.com>
2016-03-22 17:18:22 -0700

    Add variable route

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var vegetables = ['Carrot', 'Cucumber', 'Peas'];
+
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   res.send('Hello World');
@@ -7,6 +10,23 @@ app.get('/', function(req, res) {
 
 app.get('/new', function(req, res) {
   res.send('Congratulations on making a new route!');
+});
+
+app.get('/vegetables', function(req, res) {
+  res.send(vegetables.join(', '));
+});
+
+app.get('/hello/:name', function(req, res) {
+  res.send('Hello ' + req.params.name);
+});
+
+app.get('/hi', function(req, res) {
+  var name = req.query.name;
+  res.send('Hello, ' + name);
+});
+
+app.get('/*', function(req, res) {
+  res.status(404).send('Nothing here');
 });
 
 app.listen(8080, function() {
